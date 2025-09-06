97588beb6019d0e820cba121c5fd78912111aed8
Nileshdcool <neil.sukalikar@gmail.com>
2025-09-06 18:06:09 +0200

    refactor: remove .env.example and update AUTH_SECRET reference in env.js

--- src/env.js
+++ src/env.js
@@ -33,7 +33,7 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    AUTH_SECRET: process.env.AUTH_SECRET,
+    AUTH_SECRET: process.env.NEXTAUTH_SECRET,
     AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID,
     AUTH_GITHUB_SECRET: process.env.AUTH_GITHUB_SECRET,
     DATABASE_URL: process.env.MONGODB_URI,
