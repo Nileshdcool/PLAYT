988bd829c8f91119049f2fbb01e9260ed7eca93c
Nileshdcool <neil.sukalikar@gmail.com>
2025-09-08 11:51:39 +0200

    feat: add Head component for SEO and favicon configuration in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { type AppType } from "next/app";
 import { Toaster } from "react-hot-toast";
 import { Inter } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 import { api } from "~/utils/api";
 import { AppProvider } from "../AppContext";
@@ -14,6 +15,12 @@ const inter = Inter({ subsets: ["latin"] });
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <SessionProvider session={(pageProps as any).session}>
+      <Head>
+        <title>Playtastic</title>
+        <link rel="icon" type="image/svg+xml" href="/playtastic-logo.svg" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <AppProvider>
         <Toaster position="bottom-center" />
         <div className={inter.className}>
