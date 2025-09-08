414b431ba23dfde1a890da75995f11109a9114c1
Nileshdcool <neil.sukalikar@gmail.com>
2025-09-08 16:08:11 +0200

    feat: add Google Analytics tracking script to the application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,6 +20,18 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         <link rel="icon" type="image/svg+xml" href="/playtastic-logo.svg" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+          {/* Google tag (gtag.js) */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-VWNVMP678W"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-VWNVMP678W');
+              `,
+            }}
+          />
       </Head>
       <AppProvider>
         <Toaster position="bottom-center" />
